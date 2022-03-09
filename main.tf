77544df0f839ea8fbed6d62ac4eb3142723bf9c4
Nathan Parraga Thiesen <nathan.thiesen@mineiros.io>
2022-03-09 13:28:41 +0100

    feat: initial module implementation

--- main.tf
+++ main.tf
@@ -1,5 +1,51 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# THIS IS A UPPERCASE MAIN HEADLINE
-# And it continues with some lowercase information about the module
-# We might add more than one line for additional information
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ---------------------------------------------------------------------------------------------------------------------
+# Subnetwork Config
+# The default gateway automatically configures public internet access for instances with addresses for 0.0.0.0/0
+# External access is configured with Cloud NAT, which serves egress traffic for instances without external addresses
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_compute_subnetwork" "subnetwork" {
+  for_each = var.module_enabled ? 1 : 0
+
+  project     = var.project
+  network     = var.network
+  region      = var.region
+  name        = var.name
+  description = try(var.description, null)
+
+  private_ip_google_access = try(var.private_ip_google_access, true)
+  ip_cidr_range            = cidrsubnet(var.ip_cidr_range, 0, 0)
+
+  dynamic "secondary_ip_range" {
+    for_each = try(var.secondary_ip_ranges, [])
+
+    content {
+      range_name    = secondary_ip_range.value.range_name
+      ip_cidr_range = secondary_ip_range.value.ip_cidr_range
+    }
+  }
+
+  dynamic "log_config" {
+    for_each = try(var.log_config, {})
+
+    content {
+      aggregation_interval = try(each.value.aggregation_interval, null)
+      flow_sampling        = try(each.value.flow_sampling, null)
+      metadata             = try(each.value.metadata, null)
+      metadata_fields      = try(each.value.metadata_fields, null)
+      filter_expr          = try(each.value.filter_expr, null)
+    }
+  }
+
+  dynamic "timeouts" {
+    for_each = try([var.module_timeouts.google_compute_subnetwork], [])
+
+    content {
+      create = try(timeouts.value.create, null)
+      update = try(timeouts.value.update, null)
+      delete = try(timeouts.value.delete, null)
+    }
+  }
+
+  depends_on = [var.module_depends_on]
+}
