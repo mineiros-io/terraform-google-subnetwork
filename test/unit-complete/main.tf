3ae9ae387d03965b84a9463cd4bfa19bd6ee69b3
Bence Wax <bence.wax@mineiros.io>
2022-11-10 13:08:57 +0100

    chore!: update from module template

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -1,19 +1,3 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# COMPLETE FEATURES UNIT TEST
-# This module tests a complete set of most/all non-exclusive features
-# The purpose is to activate everything the module offers, but trying to keep execution time and costs minimal.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.12.0"
-    }
-  }
-}
-
-# DO NOT RENAME MODULE NAME
 module "test" {
   source = "../.."
 
@@ -41,9 +25,3 @@ module "test" {
 
   module_depends_on = ["nothing"]
 }
-
-# outputs generate non-idempotent terraform plans so we disable them for now unless we need them.
-# output "all" {
-#   description = "All outputs of the module."
-#   value       = module.test
-# }
--- test/unit-minimal/main.tf
+++ test/unit-minimal/main.tf
@@ -1,19 +1,3 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# MINIMAL FEATURES UNIT TEST
-# This module tests a minimal set of features.
-# The purpose is to test all defaults for optional arguments and just provide the required arguments.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/google"
-      version = "4.12.0"
-    }
-  }
-}
-
-# DO NOT RENAME MODULE NAME
 module "test" {
   source = "../.."
 
@@ -23,9 +7,3 @@ module "test" {
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
 }
-
-# outputs generate non-idempotent terraform plans so we disable them for now unless we need them.
-# output "all" {
-#   description = "All outputs of the module."
-#   value       = module.test
-# }
