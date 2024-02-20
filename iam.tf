aa6f406ecac389c5876ac6b8ab987af8ea2e8591
Esanim <17294241+Esanim@users.noreply.github.com>
2024-02-20 16:52:20 +0100

    fix: update child module (google-subnetwork-iam) to v.0.2.0

--- iam.tf
+++ iam.tf
@@ -15,7 +15,7 @@ locals {
 }
 
 module "iam" {
-  source = "github.com/mineiros-io/terraform-google-subnetwork-iam.git?ref=v0.1.0"
+  source = "github.com/mineiros-io/terraform-google-subnetwork-iam.git?ref=v0.2.0"
 
   for_each = var.policy_bindings == null ? local.iam_map : {}
 
@@ -34,7 +34,7 @@ module "iam" {
 }
 
 module "policy_bindings" {
-  source = "github.com/mineiros-io/terraform-google-subnetwork-iam.git?ref=v0.1.0"
+  source = "github.com/mineiros-io/terraform-google-subnetwork-iam.git?ref=v0.2.0"
 
   count = var.policy_bindings != null ? 1 : 0
 
